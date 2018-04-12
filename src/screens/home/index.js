4b4ddd6cf0482261ce1ff7d6caaa35352731df10
Justin Michot <jmichot@turner-industries.com>
2018-04-12 18:28:39 -0500

    (feat) Labor Factor's Table: hard codes data with basic logic for the Labor Factor's Table

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -2,9 +2,40 @@ import './index.scss';
 import template from './index.html';
 
 function controller() {
-  const home = this;
+  const $ctrl = this;
 
-  home.$onInit = () => {
+  $ctrl.data = [
+    {id: 1, size: 1, class: '150', schedule: '', laborFactor: 1.2},
+    {id: 1, size: 1, class: '300', schedule: '', laborFactor: 2.2},
+    {id: 1, size: 2, class: '150', schedule: '', laborFactor: 1.2},
+    {id: 1, size: 2, class: '300', schedule: '', laborFactor: 2.2},
+    {id: 1, size: 3, class: '150', schedule: '', laborFactor: 1.2},
+    {id: 1, size: 3, class: '300', schedule: '', laborFactor: 1.3},
+    {id: 1, size: 3, class: '450', schedule: '', laborFactor: 1.4},
+  ];
+
+  $ctrl.sizes = $ctrl.data.reduce((sizes, item) => {
+    return sizes.includes(item.size) ? sizes : sizes.concat(item.size);
+  }, []);
+
+  $ctrl.classes = $ctrl.data.reduce((classes, item) => {
+    return classes.includes(item.class) ? classes : classes.concat(item.class);
+  }, []);
+
+  $ctrl.schedule = $ctrl.data.reduce((schedule, item) => {
+    return schedule.includes(item.schedule) ? schedule : schedule.concat(item.schedule);
+  }, []);
+
+  $ctrl.getLaborFactor = (sch, cl, siz) =>
+    $ctrl.data.reduce((lf, item) => {
+      const {size, schedule, laborFactor} = item;
+      if ((sch == schedule || sch === undefined) && cl == item.class && siz == size) {
+        lf = laborFactor;
+      }
+      return lf;
+    }, 0);
+
+  $ctrl.$onInit = () => {
     console.log('home init');
   };
 }
